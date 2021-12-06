b39e92419370a3846221110002775a86150f5e22
Florian Samaga <florian1.samaga@st.oth-regensburg.de>
2021-12-06 11:48:15 +0100

    -bugfix sportinggrounds/models.py

--- sportinggrounds/models.py
+++ sportinggrounds/models.py
@@ -1,21 +1,21 @@
 from django.db import models
+from PIL import Image
 
 class Grounds(models.Model):
-    name = models.CharField(Sportsgroundname, max_lenght=100)
-    type = models.CharField(Type, max_lenght=100)
-    street = models.CharField(Street, max_lenght=80)
-    postal = models.CharField(Postal, max_lenght=20)
-    area = models.CharField(Area, max_length=80)
-    country = models.CharField(Country, max_lenght=60)
-    opens=models.TimeField(Opens, auto_now=False, default=None, required=False)
-    closes=models.TimeField(Closes, auto_now=False, default=None, required=False)
-    changingrooms=models.BooleanField(Changingrooms, default=False, required=False)
-    changingrooms=models.BooleanField(Changingrooms, default=False, required=False)
-    parkingsituation=models.TextField(Parkingsituation, default=None, required=False)
-    publictransportation=models.TextField(Publictransportation, default=None, required=False)
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    street = models.CharField(max_length=80)
+    postal = models.CharField(max_length=20)
+    area = models.CharField(max_length=80)
+    country = models.CharField(max_length=60)
+    opens=models.TimeField(auto_now=False, default=None, blank=True)
+    closes=models.TimeField(auto_now=False, default=None, blank=True)
+    changingrooms=models.BooleanField(default=False, blank=True)
+    parkingsituation=models.TextField(default=None, blank=True)
+    publictransportation=models.TextField(default=None, blank=True)
     image = models.ImageField(default='default.jpg', upload_to='sportground_pics')
     
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['street', 'postal', 'country', type])
+            models.UniqueConstraint(fields=['street', 'postal', 'country', 'type'], name = 'unique_ground')
         ]
